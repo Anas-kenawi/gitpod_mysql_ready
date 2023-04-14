1657bafca6da0d1af37a798706e69151a7b53358
Anas-kenawi <kenawi3@gmail.com>
2023-04-14 09:17:28 +0000

    versione non mi ricordo quanto_2

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -4,7 +4,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="CLASH_ROYALE"
+)
+mycursor= mydb.cursor()
 
 @app.route('/')
-def hello():
+def Hello():
     return render_template('hello.html', name='hgkjghjhgjgh')
+
+@app.route('/units')
+def unitList():
+    mycursor.execute("SELECT * FROM Clash_Unit")
+    myresult=mycursor.fetchall()
+    return render_template('clash_units.html', units=myresult)
